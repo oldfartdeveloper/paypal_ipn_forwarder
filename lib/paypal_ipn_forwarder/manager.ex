cf013bca9468eaef0028d32f0630671dddbc2f55
Scott Smith <scottnelsonsmith@gmail.com>
2015-09-14 20:50:00 -0700

    [#103203816] Now builds state in init() method

--- lib/paypal_ipn_forwarder/manager.ex
+++ lib/paypal_ipn_forwarder/manager.ex
@@ -5,17 +5,29 @@ defmodule PaypalIpnForwarder.Manager do
     defstruct sender_server: nil, server: nil, router: nil, client_simulator: nil
   end
 
+  ##############
+  # Client API #
+  ##############
+
   def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, [])
+  end
+
+  ####################
+  # Server Callbacks #
+  ####################
+
+  def init(_) do
     state = %State{sender_server: PaypalIpnForwarder.SenderSimulator.start_link,
                    server: PaypalIpnForwarder.Server.start_link,
                    router: PaypalIpnForwarder.Router.start_link,
                    client_simulator: PaypalIpnForwarder.ClientSimulator.start_link
                   }
-    GenServer.start_link(__MODULE__, :ok, [state])
+    {:ok, state}
   end
 
-  def init(_) do
-    {:ok, nil}
-  end
+  ####################
+  # Helper Functions #
+  ####################
 
 end
